812b9d42b6186d97eb366bfeb36028093a5cf382
vicente S. badua <95746904+badzky@users.noreply.github.com>
2024-12-18 00:02:42 +0800

    Update prelimscript.js

--- prelimscript.js
+++ prelimscript.js
@@ -2,15 +2,15 @@ document.getElementById('searchStudent').addEventListener('click', searchStudent
 
 const availableImages = {
     fruits: [
-        { path: '/verification/apple.jpg', name: 'apple' },
-        { path: '/verification/banana.png', name: 'banana' },
-        { path: '/verification/orange.jpg', name: 'orange' },
-        { path: '/verification/grapes.jpg', name: 'grapes' },
-        { path: '/verification/watermelon.jpg', name: 'watermelon' },
-        { path: '/verification/manggo.jpg', name: 'mango' },
-        { path: '/verification/strawberry.jpg', name: 'strawberry' },
-        { path: '/verification/pineapple.jpg', name: 'pineapple' },
-        { path: '/verification/kiwi.jpg', name: 'kiwi' }
+        { path: 'verification/apple.jpg', name: 'apple' },
+        { path: 'verification/banana.png', name: 'banana' },
+        { path: 'verification/orange.jpg', name: 'orange' },
+        { path: 'verification/grapes.jpg', name: 'grapes' },
+        { path: 'verification/watermelon.jpg', name: 'watermelon' },
+        { path: 'verification/manggo.jpg', name: 'mango' },
+        { path: 'verification/strawberry.jpg', name: 'strawberry' },
+        { path: 'verification/pineapple.jpg', name: 'pineapple' },
+        { path: 'verification/kiwi.jpg', name: 'kiwi' }
     ]
 };
 
